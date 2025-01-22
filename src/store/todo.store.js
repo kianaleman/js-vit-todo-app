6f2dbcd91e78d0d7dd30590080cb085903a6878e
kianaleman <kian.aleman15@gmail.com>
2025-01-21 22:46:51 -0600

    Aplicacion terminada y desplegada en netlify

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -1,7 +1,7 @@
 import { Todo } from '../todos/models/todo.model';
 
 
-const Filters = {
+export const Filters = {
     All: 'all',
     Completed: 'Completed',
     Pending: 'Pending',
@@ -12,16 +12,117 @@ const state = {
         new Todo('Piedra del alma'),
         new Todo('Piedra del infinito'),
         new Todo('Piedra del tiempo'),
+        new Todo('Piedra del poder'),
+        new Todo('Piedra del realidad'),
     ],
     filter: Filters.All,
 }
 
 
 const initStore = () => {
-    console.log(state);
+    loadStore();
+    // console.log(state);
     console.log('Init Store👾');
 }
 
-export default{
-    initStore,
+const loadStore = () => {
+    // console.log(localStorage.getItem('state'));
+
+    if(!localStorage.getItem('state')) return;
+
+    const { todos = [], filter = Filters.All } =  JSON.parse( localStorage.getItem('state') );
+    state.todos = todos;
+    state.filter = filter;
 }
+
+const saveStateToLocalStorage = () => {
+
+    // console.log( JSON.stringify(state) );
+    // localStorage.setItem('state','holaMundo');
+    localStorage.setItem( 'state', JSON.stringify(state) );
+}
+
+const getTodos = ( filter = Filters.All ) => {
+    
+    switch( filter ){
+        case Filters.All: 
+            return [...state.todos];
+        
+        case Filters.Completed:
+            return state.todos.filter( todo => todo.done );
+
+        case Filters.Pending:
+            return state.todos.filter( todo => !todo.done );
+
+        default:
+            throw new Error(`Option ${filter} is not valid.`);
+        
+    }
+}
+ 
+
+/**
+ * 
+ * @param {String} description 
+ */
+const addTodo = ( description ) => {
+    if( !description ) throw new Error('Description is required.');
+
+    state.todos.push(new Todo(description));
+    
+    saveStateToLocalStorage();
+}
+
+ 
+const toggleTodo = ( todoId ) => {
+    
+    state.todos = state.todos.map( todo => {
+        if( todo.id === todoId){
+            todo.done = !todo.done;
+        }
+        return todo;
+    })
+
+    saveStateToLocalStorage();
+}
+
+const deleteTodo = ( todoId ) => {
+    state.todos = state.todos.filter( todo => todo.id !==  todoId );
+    saveStateToLocalStorage();
+}
+
+const deleteCompleted = () => {
+    state.todos = state.todos.filter( todo => !todo.done);
+    saveStateToLocalStorage();
+}
+
+/**
+ * 
+ * @param {Filters} newFilter 
+ */
+const setFilter = ( newFilter = Filters.All ) => {
+
+    // if(Object.keys(Filters).includes(new Filters)) throw new Error(`Option ${newFilter} is not valid.`);
+    state.filter = newFilter;
+
+    saveStateToLocalStorage();
+}
+
+const getCurrentFilter = () => {
+    return state.filter;
+}
+
+
+export default{
+    deleteCompleted,
+    addTodo,
+    deleteTodo,
+    getCurrentFilter,
+    initStore,
+    getTodos,
+    loadStore,
+    setFilter,
+    toggleTodo,
+}
+
+
--- src/todos/app.js
+++ src/todos/app.js
@@ -1,5 +1,15 @@
+import todoStore, { Filters } from '../store/todo.store';
 import html from './app.html?raw'
+import { renderTodos, renderPending } from './use-cases';
 
+
+const ElementIDs = {
+    ClearCompletedButton: '.clear-completed',
+    TodoList: '.todo-list',
+    TodoFilters: '.filtro',
+    NewTodoInput: '#new-todo-input',
+    PendingCountLabel: '#pending-count',
+}
 
 /**
  * 
@@ -7,12 +17,97 @@ import html from './app.html?raw'
  */
 export const App = (elementId) => {
 
+    const displayTodos = () => {
+        const todos = todoStore.getTodos( todoStore.getCurrentFilter() );
+        renderTodos( ElementIDs.TodoList, todos );
+        updatePendingCount();
+    }
+
+
+    const updatePendingCount = () => {
+        renderPending( ElementIDs.PendingCountLabel );
+    }
+
     // Cuando la funcion App() se llama
     (() => {
         const app = document.createElement('div');
         app.innerHTML = html;
         document.querySelector(elementId).append(app);
+        displayTodos();
     })();
 
+
+    // Referencias HTML
+
+    const newDescriptionInput = document.querySelector( ElementIDs.NewTodoInput );
+    const todoListUL = document.querySelector( ElementIDs.TodoList );
+    const clearCompletedButton = document.querySelector( ElementIDs.ClearCompletedButton );
+    const FiltersLIs = document.querySelectorAll( ElementIDs.TodoFilters );
+
+
+    // Listeners
+    newDescriptionInput.addEventListener('keyup', ( event ) => {
+        // console.log(event);
+        // console.log(event.target.value);
+
+        if( event.keyCode !== 13) return;
+        if( event.target.value.trim().length === 0 ) return;
+
+        todoStore.addTodo( event.target.value );
+        displayTodos();
+        event.target.value = '';
+         
+    });
+
+
+    todoListUL.addEventListener('click', ( event ) => {
+        const element = event.target.closest('[data-id]');
+        let idTodo = element.getAttribute('data-id');
+
+        todoStore.toggleTodo( idTodo );
+        displayTodos();
+    });
+
+    todoListUL.addEventListener('click', ( event ) => {
+        if(!event.target.classList.contains('destroy')) return;
+        const element = event.target.closest('[data-id]');
+        let idTodo = element.getAttribute('data-id');
+
+        todoStore.deleteTodo( idTodo );
+        displayTodos();
+    });
+
+    clearCompletedButton.addEventListener('click', () => {
+        // const deleteCompleted = event.target.className === 'clear-completed';
+        // if( !deleteCompleted ) return;
+        todoStore.deleteCompleted();
+        displayTodos();
+    });
+
+    FiltersLIs.forEach( element =>{
+        element.addEventListener( 'click', ( element ) => {
+            FiltersLIs.forEach( el => el.classList.remove('selected'));
+            element.target.classList.add('selected');
+            switch(element.target.text){
+                case 'Todos': 
+                    todoStore.setFilter( Filters.All );
+                break;
+
+                case 'Pendientes':
+                    todoStore.setFilter( Filters.Pending );
+                break;
+
+                case 'Completados':
+                    todoStore.setFilter( Filters.Completed );
+                break;
+                
+            }
+
+            displayTodos();
+        } );
+
+        
+
+    });
 
 }
